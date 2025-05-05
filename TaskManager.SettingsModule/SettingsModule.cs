40bc089d715ce5a7a80ba796fb7ffd6106af2471
AGANETHED <astcvetkov1@chsu.ru>
2025-05-05 17:20:01 +0300

    Модуль настроек

---
+++ TaskManager.SettingsModule/SettingsModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.SettingsModule.View;
+using TaskManager.SettingsModule.ViewModels;
+
+namespace TaskManager.SettingsModule
+{
+    public class SettingsModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            //var regionManager = containerProvider.Resolve<IRegionManager>();
+            //regionManager.RequestNavigate("MainRegion", "SettingsView");
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<SettingsView, SettingsViewModel>("SettingsView");
+        }
+    }
+}
